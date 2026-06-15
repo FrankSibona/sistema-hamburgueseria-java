0a2d7f1bf63dbd3448c12ec916db6344fcb162e7
MarcosGonzalez <marcosgantonio2007@gmail.com>
2026-06-14 21:12:38 -0300

    Fronted/cart

--- src/Views/Products.java
+++ src/Views/Products.java
@@ -1,4 +1,21 @@
 // Frontend de los Productos
 public class Products {
-    
+    private String name;
+    private double price;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
 }
